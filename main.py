38ef92591e01689e96a2cd3e6db46870106cc779
dilyanabodurova <diliana.bodurova@abv.bg>
2025-12-31 19:54:58 +0200

    -

--- main.py
+++ main.py
@@ -3,24 +3,25 @@ from tkinter import *
 BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
-window.title("Widget Examples")
+window.title("Flash Cards")
 window.config(padx=50, pady=50)
 
-card_front_image=PhotoImage(file="images/card_front.png")
-flash_card = Canvas(height=526, width=800)
-flash_card.create_image(100, 100, image=card_front_image)
+card_front_image = PhotoImage(file="images/card_front.png")
+canvas = Canvas(height=526, width=800)
+canvas.create_image(526, 800, image=card_front_image)
 
+label_language = canvas.create_text(400, 150, text="French", font=('Arial', 40, 'italic'), fill="white")
+canvas.grid(row=0, column=0, columnspan=2)
 
+label_language_translate = canvas.create_text(400, 263, text="trouve", font=('Arial', 60, 'bold'), fill="white")
+canvas.grid(row=0, column=0, columnspan=2)
 
-right_image=PhotoImage(file="images/right.png")
-right_button = Button(image=right_image,bg=BACKGROUND_COLOR)
-right_button.grid(row=1,column=1)
+right_image = PhotoImage(file="images/right.png")
+right_button = Button(image=right_image, bg=BACKGROUND_COLOR)
+right_button.grid(row=1, column=1)
 
-wrong_image=PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image,bg=BACKGROUND_COLOR)
-wrong_button.grid(row=1,column=0)
+wrong_image = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong_image, bg=BACKGROUND_COLOR)
+wrong_button.grid(row=1, column=0)
 
-
-label_language = Label(text="French", font=("Arial", 40, "italic"),fg="black",bg=BACKGROUND_COLOR)
-label_language.grid(row=0,column=0)
 window.mainloop()
